e1f689946f4af072fd61cec8c6adfea036f8224a
Lucas Wozniak <lucas.wozniak1@gmail.com>
2025-07-13 08:52:08 -0700

    Fix scenes 1-8 figure not appearing, final build for City of Water day

--- GowanusAR_2025_UnityProject/Assets/Scripts/ForceRotateArmature_Canal_Onsite.cs
+++ GowanusAR_2025_UnityProject/Assets/Scripts/ForceRotateArmature_Canal_Onsite.cs
@@ -2,14 +2,11 @@ using UnityEngine;
 
 public class ForceRotateArmature_Canal_Onsite : MonoBehaviour
 {
+
+    private float time = 0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         if (this.gameObject.transform.localRotation != Quaternion.Euler(0, -30, 0))
         {
@@ -17,6 +14,32 @@ public class ForceRotateArmature_Canal_Onsite : MonoBehaviour
             // Force the rotation to be zero
             //ForceRotation();
         }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time > 5f)
+        {
+            if (this.gameObject.transform.localRotation != Quaternion.Euler(0, -30, 0))
+            {
+                this.gameObject.transform.localRotation = Quaternion.Euler(0, -30, 0);
+                // Force the rotation to be zero
+                //ForceRotation();
+            }
+            time = 0f; // Reset the timer
+            this.enabled = false; // Disable this script after the first 5 seconds
+        }
+
         
+        /*if (this.gameObject.transform.localRotation != Quaternion.Euler(0, -30, 0))
+        {
+            this.gameObject.transform.localRotation = Quaternion.Euler(0, -30, 0);
+            // Force the rotation to be zero
+            //ForceRotation();
+        }*/
+
     }
 }
